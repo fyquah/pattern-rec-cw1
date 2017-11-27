737f718e2ae62e59c283e6c4787fae2b1927db8f
Fu Yong Quah <quah.fy95@gmail.com>
2017-11-27 14:48:54 +0000

    Add eigenfaces

---
+++ eigenfaces_2.m
@@ -0,0 +1,17 @@
+function [eigenvectors, eigenvalues, mean_face] = eigenfaces(X)
+    N = size(X, 2);
+    mean_face = 1 / N * mean(X, 2);
+    
+    % 4. substract mean face
+    A = X - mean_face;
+    size(mean_face) 
+    
+    % 5. Covariance matrix
+    S = 1.0 / N * A' * A;
+    
+    % 6. Compute eigen values (doesn't actually take that long to compute)
+    [eigenvectors, eigenvalues] = eig(S);
+    
+    % Since it takes awhile to compute, save them somewhere
+    save("eigenfaces_2.mat", "eigenvectors", "eigenvalues", "mean_face");
+end
