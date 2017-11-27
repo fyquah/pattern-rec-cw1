19650a8437cff713c5d748bbb0af6c2e64825253
Fu Yong Quah <quah.fy95@gmail.com>
2017-11-27 14:23:31 +0000

    regenerate training data

--- generate_training_data.m
+++ generate_training_data.m
@@ -2,4 +2,4 @@
 
 load("face.mat");
 [X_train, X_test, y_train, y_test] = get_training_data(X, 0.7, 10);
-save("partitioned.mat", 'X_train', 'X_test', 'y_train', 'y_test');
+save("data_partition.mat", 'X_train', 'X_test', 'y_train', 'y_test');
