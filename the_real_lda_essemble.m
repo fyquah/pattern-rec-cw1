a2db0af60544bdad9c322c95c015288b47a61637
Fu Yong Quah <quah.fy95@gmail.com>
2017-12-17 21:57:23 +0000

    commit

--- the_real_lda_essemble.m
+++ the_real_lda_essemble.m
@@ -19,10 +19,31 @@ end
 X_train_lda = W' * X_train_pca;
 
 X_test_pca = ((X_test - meanface2)'*U)';
+X_validation_pca = ((X_validation' - meanface2)'*U)';
 
 test_size = size(X_test_pca,2);
 
-y_guess = zeros(1,test_size);
+y_test_guess = zeros(1, test_size);
+y_validation_guess = zeros(1, 52);
+validation_size = 52;
+
+for i=1:validation_size
+    proj = W(:,1:lda_dim)'*X_validation_pca(:,i);
+    dists = zeros(1,52 * 7);
+    
+%     for j = 1:N;
+%         dists(j) = pdist2(means_pca(j,1:lda_dim),proj');
+%     end
+    
+    for t = 1:size(X_train_lda, 2)
+      d = (X_train_lda(1:lda_dim, t) - proj)';
+      dists(:, t) = sum(d .^ 2);
+    end
+    
+    
+    [dist, bla] = min(dists);
+    y_validation_guess(i) = y_train(bla);
+end
 
 for i=1:test_size
     proj = W(:,1:lda_dim)'*X_test_pca(:,i);
@@ -39,8 +60,10 @@ for i=1:test_size
     
     
     [dist, bla] = min(dists);
-    y_guess(i) = y_train(bla);
+    y_test_guess(i) = y_train(bla);
 end
 
-fprintf(" - lda_dim = %d accuracy = %.3f\n", ...
-    lda_dim, sum(y_guess == y_test) / length(y_test));
+fprintf(" - lda_dim = %d validation = %.3f, test = %.3f\n", ...
+    lda_dim, ...
+    sum(y_validation_guess == y_validation) / length(y_validation_guess), ...
+    sum(y_test_guess == y_test) / length(y_test_guess));
--- vecnorm.m
+++ vecnorm.m
@@ -1,5 +1,5 @@
 function [res] = vecnorm(vec, p, dim)
-    N = size(vec,2)
+    N = size(vec,2);
     tmp = zeros(1,N);
     for i=1:N
         tmp(i) = norm(vec(:,i),2);
